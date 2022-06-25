ea2e1079f6d4658ca275a8dfe30b674a569c452c
Deniskinyua <kimenjudenis@gmail.com>
2022-06-25 12:02:25 +0300

    learning

---
+++ indexed_collections/array_objects/includes.js
@@ -0,0 +1,37 @@
+/**
+ * method includes();
+ * 
+ * syntax: (i) .includes(searchEelement), (ii) .includes(searchEelement, fromIndex)
+ */
+
+
+// Declare variable
+let word = 'Goodmorning my neighbours. I am going to work';
+
+//Using syntax: (i) .includes(searchEelement)
+
+//Just confirming
+console.log(word.includes('Goodbye'));
+
+
+//function with if/else statements
+function chekingIncludes(word) {
+    if (word.includes('Goodbye')) {
+        return word;
+}
+    else{
+        return word + '.Goodbye!';
+    }
+}
+console.log(chekingIncludes(word));
+
+// Rather precise function 
+function chekingTncl (word) {
+    return word.includes('.Hooray!') ? word : word + '.Hooray!';
+}
+console.log(chekingTncl(word));
+
+//Using syntaxt: includes(searchElement, fromIndex);
+console.log(word.includes('Goodmorning', 9));
+
+
