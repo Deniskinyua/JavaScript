9cfd703e7d9ead8dbd147562e5e1a2c9fbce4ad3
Deniskinyua <kimenjudenis@gmail.com>
2022-08-27 06:51:24 +0300

    subtract

---
+++ JavaScript-testing/subtract/subtract.js
@@ -0,0 +1,5 @@
+// Function subtracting two numbers
+function subtract(a,b){
+    return a-b;
+}
+module.exports = subtract;
---
+++ JavaScript-testing/subtract/subtract.test.js
@@ -0,0 +1,4 @@
+const subtract = require ('./subtract')
+test('Subtracts two numbers', () =>{
+    expect(subtract(2,1)).toBe(1)
+})
